2081433498eaa2d8535a11ad444b7236d25994a2
Christian Lim <clim@pop-os.localdomain>
2021-12-01 23:25:31 -0600

    small update

--- src/util/api_util.py
+++ src/util/api_util.py
@@ -4,9 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AV_API_KEY = os.getenv("AV_API_KEY", "value does not exist")
-# AV_API_KEY = "CH447Y09NPSTFX3A"
-# AV_API_KEY_2 = "5P5X5CSURTZ48C8X"
-
-# # BINANCE US
-# BINANCE_KEY = "Gc1m8jMmDx2cHcB44CrHXhJaiigSGKtGcWu8zvaAGajK2D3MzQ0GnBc8ye0IbF6L"
-# BINANCE_SECRET = "gtavZAx9IUtd6TUIjG63BQb0vt8mrnKJkGgCL3eMG6aUII7dvddyQXx4dboU2TPj"
+AV_API_KEY_2 = os.getenv("AV_API_KEY_2", "value does not exist")
