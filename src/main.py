a8eea45b3560a369b186c9971b01351a7a900d13
Christian Lim <christiananthonylim1@gmail.com>
2021-12-13 19:56:10 -0600

    Added bbands

--- src/main.py
+++ src/main.py
@@ -3,26 +3,14 @@ import pandas as pd
 import numpy as np
 from stock.lib.daily import daily_adjusted_ohlc
 from stock.lib.intraday import intraday_ohlc
-from stock.lib.technicals.indicators import sma
+from stock.lib.technicals.indicators import sma, bbands
 from stock.lib.fundamentals.company_info import * 
 from stock.lib.fundamentals.earnings import *
 
 
 def main():
-    # js = intraday_ohlc(symbol="NVDA", interval="1min")
-    # js = daily_adjusted_ohlc("QRVO")
-    # s = sma("QRVO", "daily", 200, "close")
-    # df = js.join(s, how="inner", lsuffix='x')
-    # df.drop(inplace=True, columns=["Symbolx"])
-    # print(js)
-    # client = BinanceClient()
-    # df = get_all_coins(client)
-    # sol = get_coin_info(client, coin='SOLUSD')
-    # print(df)
-    # df = get_annual_income_statements('NVDA')
-    # earn = get_quarterly_earnings("NVDA")
-    info = company_info("NVDA")
-    print(df)
+    d = bbands("NVDA", "5min")
+    print(d)
 
 
 if __name__ == "__main__":
--- src/stock/lib/technicals/indicators.py
+++ src/stock/lib/technicals/indicators.py
@@ -3,12 +3,13 @@ import pandas as pd
 from util.api_util import AV_API_KEY
 
 
-def sma(symbol, interval, time_period, series_type):
+def sma(symbol, interval, time_period=20, series_type='close'):
     """
     Returns a simple moving average
 
     Interval: 1min, 5min, 15min, 30min, 60min, daily, weekly, monthly
     Time_period: Number of datapoints used to calculate each moving avg value.
+    series_type: The desired price type in the time series. Four supported: close, open, high, low
     """
 
     url = f"https://www.alphavantage.co/query?function=SMA&symbol={symbol}&interval={interval}&time_period={time_period}&series_type={series_type}&apikey={AV_API_KEY}"
@@ -21,3 +22,31 @@ def sma(symbol, interval, time_period, series_type):
     df = df.rename(columns={"SMA": f"SMA{time_period}"})
     df['symbol'] = symbol
     return df
+
+
+def bbands(symbol, interval, series_type='close', time_period=20, std_dev_up=2, std_dev_down=2, matype=0):
+    """
+    Returns bollinger bands 
+    Goes back 25 days (cal days)
+
+    Interval: Time interval between consecutive data points in the time series.
+    - 1min, 5min, 15min, 30min, 60min, daily, weekly, monthly
+
+    time_period: The number of datapoints used to calculat each bbands value
+
+
+    """
+    url = f"https://www.alphavantage.co/query?function=BBANDS&symbol={symbol}&interval={interval}&time_period={time_period}&series_type={series_type}&nbdevup={std_dev_up}&nbdevdn={std_dev_down}&apikey={AV_API_KEY}"
+    r = requests.get(url)
+    data = r.json()
+    bb = data["Technical Analysis: BBANDS"]
+    df = pd.DataFrame(bb).T
+    df = df.rename(columns={
+        "Real Upper Band": "upper_bound",
+        "Real Lower Band": "lower_bound",
+        "Real Middle Band": "mid_bb"
+    })
+    df["symbol"] = symbol
+    return df
+
+
